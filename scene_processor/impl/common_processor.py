e7086140b638e7014b3956040f45e575c3b5c122
yinghaoxuan <haying@student.unimelb.edu.au>
2024-03-08 15:16:11 +0800

    新增逻辑判断对应场景是否需要调用api进行词槽填充

--- scene_processor/impl/common_processor.py
+++ scene_processor/impl/common_processor.py
@@ -6,6 +6,7 @@ from scene_processor.scene_processor import SceneProcessor
 from utils.helpers import get_raw_slot, update_slot, format_name_value_for_logging, is_slot_fully_filled, send_message, \
     extract_json_from_string, get_dynamic_example
 from utils.prompt_utils import get_slot_update_message, get_slot_query_user_message
+from utils.send_llm import fetch_decision_from_api
 
 
 class CommonProcessor(SceneProcessor):
@@ -32,7 +33,16 @@ class CommonProcessor(SceneProcessor):
         if is_slot_fully_filled(self.slot):
             return self.respond_with_complete_data()
         else:
-            return self.ask_user_for_missing_data(user_input)
+             # 分析缺失的数据，并决定是向用户询问还是调用API
+            missing_data_action = self.decide_next_action(self.slot, user_input)
+        
+            if missing_data_action == "ask_user":
+                return self.ask_user_for_missing_data(user_input)
+            elif missing_data_action == "call_api":
+                return self.fetch_data_from_api(self.slot)
+            else:
+                # 默认行为或错误处理
+                return "无法确定下一步行动，请手动检查数据。"
 
     def respond_with_complete_data(self):
         # 当所有数据都准备好后的响应
@@ -46,3 +56,29 @@ class CommonProcessor(SceneProcessor):
         # 请求用户填写缺失的数据
         result = send_message(message, user_input)
         return result
+    
+    # def fetch_data_from_external_source(self, slot_name, context):
+
+    # #根据槽位名称和上下文调用外部API获取数据
+    # # 根据slot_name和context确定要调用的API以及所需的参数
+    # api_config = self.determine_api_config(slot_name, context)
+    # if api_config is None:
+    #     return None
+
+    # # 调用API并获取数据
+    # response_data = self.call_external_api(api_config)
+    # if response_data:
+    #     # 处理响应数据并更新槽位
+    #     self.update_slot_with_external_data(response_data, slot_name)
+    #     return True
+    # else:
+    #     return False
+
+    def decide_next_action(self, slot, user_input):
+        result = fetch_decision_from_api(self, slot, user_input)
+        logging.info(f"Decided next action: {result}")
+        return result
+
+    def fetch_data_from_api(self, slot):
+        # 具体调用api的方法
+        pass
